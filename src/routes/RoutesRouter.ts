4fca9f46cac7570a53d8f78ee701823524e6dfa7
Geraldine Granada <gkgranada@gmail.com>
2018-06-04 04:11:20 +0300

    Add basic functionality for getting best route - doesn't return proper response yet

--- src/routes/RoutesRouter.ts
+++ src/routes/RoutesRouter.ts
@@ -2,62 +2,87 @@ import {Router, Request, Response, NextFunction} from 'express';
 import db = require('../db/RoutesQueries');
 import{Airport} from '../models/Airport';
 import {Route} from '../models/Route';
+import {AStarRouteSearch} from '../helpers/AStarRouteSearch';
 
 export class RoutesRouter {
-	router: Router
+    router: Router
 
-	constructor() {
-		this.router = Router();
-		this.init();
-	}
+    constructor() {
+        this.router = Router();
+        this.init();
+    }
 
-	/**
-	 * Get all Routes
-	 */
-	public getRoutes(req: Request, res: Response, next: NextFunction) {
-		db.routes.all(req, res, next);
-	} 
+    /**
+     * Get all Routes
+     */
+    public getRoutes(req: Request, res: Response, next: NextFunction) {
+        db.routes.all(req, res, next)
+        .then(function (data) {
+          res.status(200)
+            .json({
+              status: 'success',
+              data: data,
+              message: 'Retrieved ALL routes'
+            });
+        })
+        .catch(function (err) {
+          return next(err);
+        });
+    }
 
 
-	/**
-	 * Get Best Route
-	 */
-	public getBestRoute(req: Request, res: Response, next: NextFunction) {
-		let origin = req.query.origin;
-		let dest = req.query.destination;
-		console.log('origin: ' + origin)
-		console.log('dest: ' + dest)
+    /**
+     * Get Best Route
+     */
+    public getBestRoute(req: Request, res: Response, next: NextFunction) {
+        let origin = req.query.origin;
+        let dest = req.query.destination;
+        console.log('origin: ' + origin);
+        console.log('dest: ' + dest);
+        let vertices:Map<number, Airport> = new Map<number, Airport>();
+        let edges:Map<number, number[]> = new Map<number, number[]>();
 
-		let oAirport = db.airports.findByCode(origin);
-		let dAirport = db.airports.findByCode(dest);
+        let originPromise = db.airports.findByCode(origin);
+        let destPromise = db.airports.findByCode(dest);
 
-		let vertices:Map<number, Airport> = new Map<number, Airport>();
-		let airports = db.airports.all(req, res, next).data;
-		for (let a in airports) {
-			let airport = JSON.parse(a)
-			vertices.set(airport.id, airport);
-		}
+        let verticesPromise = db.airports.all();
+        let edgesPromise = db.routes.all(req, res, next);
 
-		let routes = db.routes.all(req, res, next).data;
-		let edges:Map<number, number[]> = new Map<number, number[]>();
-		for (let r in routes) {
-			let route = JSON.parse(r);
-			if (edges[route.sourceid] == undefined) {
-				edges.set(route.sourceid, [route.destid]);
-			} else {
-				edges[route.sourceid].push(route.destid);
-			}
-		}
+        Promise.all([originPromise, destPromise, verticesPromise, edgesPromise]).then(function(values) {
+            console.log(values[0]);
+            console.log(values[1]);
+            values[2].forEach(function(a){
+                vertices.set(a.id, a);
+            });
+            //console.log(vertices);
+            values[3].forEach(function(route){
+                let src = parseInt(route.sourceid);
+                let dst = parseInt(route.destid);
+                if (undefined == edges.get(src)) {
+                    edges.set(src, [dst]);
+                } else {
+                    edges.get(src).push(dst);
+                }
+            });
+            //console.log(edges);
 
-		console.log('edges: ' + edges)	
-	} 
-	/**
-	 * Attach handlers to ExpressRouter's endpoints
-	 */
-	init() {
-		this.router.get('/', this.getRoutes)
-		this.router.get('/best/', this.getBestRoute)
-	}
+            let path = AStarRouteSearch.getShortestPath(
+                    values[0].id,
+                    values[1].id,
+                    vertices,
+                    edges);
+            console.log(path);
+        });
+
+    }
+
+    /**
+     * Attach handlers to ExpressRouter's endpoints
+     */
+    init() {
+        this.router.get('/', this.getRoutes)
+        this.router.get('/best/', this.getBestRoute)
+    }
 
 
 }
