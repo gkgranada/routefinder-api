3d57e5955157378f3a63bd13ead7448fa48acb71
Geraldine Granada <gkgranada@gmail.com>
2018-06-05 15:38:34 +0300

    Construct list of Routes from list of airport nodes

--- src/helpers/RouteHelper.ts
+++ src/helpers/RouteHelper.ts
@@ -1,8 +1,18 @@
 import {Route} from '../models/Route';
 
 module RouteHelper {
-	export function constructFlightPath(routes : Map<number, Route[]>, path : number[]) : Route[] {
+    export function constructFlightPath(routes : Map<number, Route[]>, path : number[]) : Route[] {
     let finalRoute : Route[] = [];
+    for ( let i = 0; i < path.length - 1; i++) {
+        let edges = routes.get(path[i]);
+        for (let j = 0; j < edges.length; j++) {
+            let edge = edges[j];
+            if (edge.destid == path[i+1].toString()) {
+                finalRoute.push(edge); // only get first instance in case of multiple flight options
+                break;
+            }
+        }
+    }
     return finalRoute;
 }
 }
